0c96f7df50640d9045d1d9afb1bc881f01a8fe1a
Max Raab <max.raab@maxraab.de>
2015-05-09 21:38:51 +0200

    SVM der Demo-Anwendung aktualisiert

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -12,13 +12,13 @@ namespace Demo
                 //Args arg = new Args("l,n*,a#", args);
                 Args arg = new Args("l,n*,a#", new string[] { "-n", "Max", "-a", "25", "-l" });
 
-                Console.WriteLine("soll geloggt werden?: {0}", arg.GetBoolean('l'));
-                Console.WriteLine("dein Name: {0}", arg.GetString('n'));
-                Console.WriteLine("dein Alter in Jahren: {0}", arg.GetInteger('a'));
+                Console.WriteLine("soll ein Log geschrieben werden?: {0}", arg.GetBoolean('l'));
+                Console.WriteLine("Name: {0}", arg.GetString('n'));
+                Console.WriteLine("Alter: {0}", arg.GetInteger('a'));
             }
             catch (ArgsException e)
             {
-                Console.WriteLine("Argument error: {0}", e.Message);
+                Console.WriteLine("Fehler: {0}", e.Message);
             }
         }
     }
--- Utilities/Args.cs
+++ Utilities/Args.cs
@@ -72,11 +72,6 @@ namespace Utilities
                 {
                     ParseArgumentCharacters(argsString.Substring(1));
                 }
-                //else
-                //{
-                //    _currentArgument = previousArgument;
-                //    break;
-                //}
             }
         }
 
