cd9478893a30c42afbab64ed551a7ade61174eaa
Max Raab <max.raab@maxraab.de>
2015-05-09 18:04:29 +0200

    ErrorCode und ArgsException hinzugefügt

--- Utilities/Args.cs
+++ Utilities/Args.cs
@@ -1,7 +1,52 @@
 ﻿
+using System.Collections.Generic;
 namespace Utilities
 {
     public class Args
     {
+        private Dictionary<char, IArgumentMarshaler> _marshalers;
+        private List<char> _argsFound;
+        private IEnumerable<string> currentArgument;
+
+        public Args(string shema, string[] args)
+        {
+            _marshalers = new Dictionary<char, IArgumentMarshaler>();
+            _argsFound = new List<char>();
+
+            ParseSchema(shema);
+            ParseArgumentStrings(new List<string>(args));
+        }
+
+        private void ParseSchema(string schema)
+        {
+            foreach (var element in schema.Split(','))
+            {
+                if (element.Length > 0)
+                {
+                    ParseSchemaElement(element.Trim());
+                }
+            }
+        }
+
+        private void ParseSchemaElement(string element)
+        {
+            var elementId = element[0];
+            var elementTail = element.Substring(1);
+
+            ValidateSchemaElementId(elementId);
+        }
+
+        private void ValidateSchemaElementId(char elementId)
+        {
+            if (!char.IsLetter(elementId))
+            {
+                throw new ArgsException(ErrorCode.Invalid_Argument_Name, elementId, null);
+            }
+        }
+
+        private void ParseArgumentStrings(List<string> list)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
---
+++ Utilities/ErrorCode.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Utilities
+{
+    public enum ErrorCode
+    {
+        Ok,
+        Invalid_Argument_Format,
+        Unexpected_Argument,
+        Invalid_Argument_Name,
+        Missing_String,
+        Missing_Integer,
+        Invalid_Integer,
+        Missing_Double,
+        Invalid_Double,
+    }
+}
